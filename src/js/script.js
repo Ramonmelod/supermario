b65bbb85207e767b6b83ec3c56e412ddeb0182e5
Ramon Melo <139141993+Ramonmelod@users.noreply.github.com>
2023-10-20 13:25:22 -0300

    Pontuação implementada

--- src/js/script.js
+++ src/js/script.js
@@ -1,6 +1,8 @@
 const mario = document.querySelector('.mario');
 const pipe = document.querySelector('.pipe');
 const gameOver = document.querySelector('.gameOver');
+let pontos = 0;
+let pontosControle = true;                         // apenas para controle do registro de pontuação
 
 const jump = ()=>{
     mario.classList.add('jump');
@@ -12,18 +14,26 @@ const jump = ()=>{
 
 document.addEventListener('keydown',jump);
 
-const loop = setInterval (() =>{
-    const pipePosition = pipe.offsetLeft;        // monitora o posicionamento class pipe
-    const marioPosition = mario.offsetTop;       //monitora o posicionamento class mario
+const loop1 = setInterval (() =>{
     
-    if((pipePosition < 68 ) & (marioPosition > 398)){
-    pipe.style.animation = 'none';
-    gameOver.style.bottom = marioPosition - 350 + 'px'; // concatenação - 350
-    gameOver.style.display ='block';
-    mario.style.display = 'none';
-    pipe.style.left = pipePosition + 'px';      //concatenação de pipepisitioncom px
-
+    const pipePosition = pipe.offsetLeft;        // monitora o posicionamento class pipe
+    const marioPosition = mario.offsetTop;         //monitora o posicionamento class mario
+        
+    if((pipePosition < 68)&&(marioPosition < 243) &&(pontosControle)){  // condição de pontuação
+             pontos++;
+             const mostrarPontos = document.getElementsByClassName('pontuacao')[0];// recebe o primeiro elemento da classe pontos
+             mostrarPontos.innerHTML = pontos;                       // altera o mostrador dos pontos
+    }
+    else if((pipePosition < 68 ) & (marioPosition > 398)){  // condição de game over
+            
+            pipe.style.animation = 'none';                       // desliga o movimento do cano
+            gameOver.style.bottom = marioPosition - 350 + 'px'; // concatenação - 350
+            gameOver.style.display ='block';                    // mostra o desenho de game-over do mario
+            mario.style.display = 'none';                       // esconde o gif do mario andando
+            pipe.style.left = pipePosition + 'px';             //concatenação de pipepisitioncom px
+            pontosControle = false;
+    
     
 }
-},10);
+},100);
 
