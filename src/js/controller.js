c41fa9a48b0e15e6c5ce74f3ef2e4aa85aa1d38a
Ramonmelod <ramonmelo.com@gmail.com>
2024-07-02 15:59:56 -0300

    change of the names of the variable from function controller

--- src/js/controller.js
+++ src/js/controller.js
@@ -1,18 +1,18 @@
 import { pontosIncremento, pontosControleAlter } from "./script.js";
 export const controller = (
-  mario,
-  pipe,
-  gameOver,
-  dialogo,
-  pontos,
-  animationTime,
-  pontosControle,
-  mostrarPontos
+  mariocontroller,
+  pipecontroller,
+  gameOvercontroller,
+  dialogocontroller,
+  pontoscontroller,
+  animationTimecontroller,
+  pontosControlecontroller,
+  mostrarPontoscontroller
 ) => {
   const jump = () => {
-    mario.classList.add("jump");
+    mariocontroller.classList.add("jump");
     setTimeout(() => {
-      mario.classList.remove("jump"); //setTimeout faz com que o código espere algum tempo até ir para  a função anônima
+      mariocontroller.classList.remove("jump"); //setTimeout faz com que o código espere algum tempo até ir para  a função anônima
     }, 1000);
   };
 
@@ -23,28 +23,28 @@ export const controller = (
   });
 
   const loop1 = setInterval(() => {
-    const pipePosition = pipe.offsetLeft; // monitora o posicionamento class pipe
-    const marioPosition = mario.offsetTop; //monitora o posicionamento class mario
-    animationTime = animationTime - 0.0005; //decremento da variavel animationTime. Isto acelera o cano
-    pipe.style.animation = `pipe-animation ${animationTime}s infinite linear`;
+    const pipePosition = pipecontroller.offsetLeft; // monitora o posicionamento class pipecontroller
+    const marioPosition = mariocontroller.offsetTop; //monitora o posicionamento class mariocontroller
+    animationTimecontroller = animationTimecontroller - 0.0005; //decremento da variavel animationTimecontroller. Isto acelera o cano
+    pipecontroller.style.animation = `pipe-animation ${animationTimecontroller}s infinite linear`;
 
-    if (pipePosition < 65 && marioPosition < 353 && pontosControle) {
-      console.log(pontosControle);
-      mostrarPontos.innerHTML = pontosIncremento(); // altera o mostrador dos pontos esta função é exportada do script.js
+    if (pipePosition < 65 && marioPosition < 353 && pontosControlecontroller) {
+      console.log(pontosControlecontroller);
+      mostrarPontoscontroller.innerHTML = pontosIncremento(); // altera o mostrador dos pontos esta função é exportada do script.js
     } else if ((pipePosition < 65) & (marioPosition > 353)) {
       // condição de game over
 
-      pipe.style.animation = "none"; // desliga o movimento do cano
-      gameOver.style.bottom = marioPosition - 330 + "px"; // concatenação - 350
-      gameOver.style.display = "block"; // mostra o desenho de game-over do mario
-      mario.style.display = "none"; // esconde o gif do mario andando
-      pipe.style.left = pipePosition + "px"; //concatenação de pipeposition com px
+      pipecontroller.style.animation = "none"; // desliga o movimento do cano
+      gameOvercontroller.style.bottom = marioPosition - 330 + "px"; // concatenação - 350
+      gameOvercontroller.style.display = "block"; // mostra o desenho de game-over do mariocontroller
+      mariocontroller.style.display = "none"; // esconde o gif do mariocontroller andando
+      pipecontroller.style.left = pipePosition + "px"; //concatenação de pipeposition com px
       pontosControleAlter(); // função exportada do script.js
-      console.log("gameOver " + pontosControle);
-      pontosControle = false;
-      console.log("gameOver " + pontosControle);
-      dialogo.style.display = "block";
+      console.log("gameOver " + pontosControlecontroller);
+      pontosControlecontroller = false; //altera a variável pontosControlecontroller para que a condição que altera o mostrador dos pontos não seja incrementada
+      console.log("gameOver " + pontosControlecontroller);
+      dialogocontroller.style.display = "block";
     }
   }, 100);
-  return pontos;
+  return pontoscontroller;
 };
--- src/js/script.js
+++ src/js/script.js
@@ -36,35 +36,6 @@ controller(
   mostrarPontos
 );
 
-/*
-//------------------------movimento-------------------------------------
-const loop1 = setInterval(() => {
-  const pipePosition = pipe.offsetLeft; // monitora o posicionamento class pipe
-  const marioPosition = mario.offsetTop; //monitora o posicionamento class mario
-
-  animationTime = animationTime - 0.0005; //decremento da variavel animationTime. Isto acelera o cano
-  pipe.style.animation = `pipe-animation ${animationTime}s infinite linear`;
-
-  if (pipePosition < 65 && marioPosition < 353 && pontosControle) {
-    // condição de pontuação
-    pontos++;
-    const mostrarPontos = document.getElementsByClassName("pontuacao")[0]; // recebe o primeiro elemento da classe pontos
-    mostrarPontos.innerHTML = pontos; // altera o mostrador dos pontos
-  } else if ((pipePosition < 65) & (marioPosition > 353)) {
-    // condição de game over
-
-    pipe.style.animation = "none"; // desliga o movimento do cano
-    gameOver.style.bottom = marioPosition - 330 + "px"; // concatenação - 350
-    gameOver.style.display = "block"; // mostra o desenho de game-over do mario
-    mario.style.display = "none"; // esconde o gif do mario andando
-    pipe.style.left = pipePosition + "px"; //concatenação de pipeposition com px
-    pontosControle = false;
-    dialogo.style.display = "block";
-  }
-}, 100);*/
-
-//------------------------movimento-------------------------------------
-
 let dataRecordistas = [];
 
 for (let i = 1; i < 11; i++) {
